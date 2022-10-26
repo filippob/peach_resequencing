81c54f5ceaf951548e211086089961605260a2f7
filippob <filippo.biscarini@gmail.com>
2022-10-26 13:25:38 +0200

    replacing csv.DictWriter with Pandas .to_csv

--- python_scripts/make_sample_sheet.py
+++ python_scripts/make_sample_sheet.py
@@ -1,7 +1,9 @@
 
 import re
 import csv
+import pandas as pd
 from pathlib import Path
+from collections import OrderedDict
 
 path_to_files = '/home/ngs/freeclimb_resequencing/BGI-FreeClimb'
 outdir = '/home/freeclimb/Config'
@@ -27,16 +29,19 @@ for smp in range(len(sample_folders)):
             temp_dict['fastq_2'] = x
 
     sample_dict.append(temp_dict)
+    #sample_dict.append(OrderedDict(sorted(temp_dict.items())))
 
 print('n. of records in sample_dict:',len(sample_dict))
-print(sample_dict[0])
-
-
-keys = sample_dict[0].keys()
 
 fname = Path(outdir).joinpath('prova.csv')
-with open(fname, 'w', newline='') as output_file:
-    dict_writer = csv.DictWriter(output_file, keys)
-    dict_writer.writeheader()
-    dict_writer.writerows(sample_dict)
+#with open(fname, 'w', newline='') as output_file:
+#    dict_writer = csv.DictWriter(output_file, keys)
+#    dict_writer.writeheader()
+#    dict_writer.writerows(sample_dict)
+
+
+df = pd.DataFrame(sample_dict)
+cols = ['sample', 'fastq_1', 'fastq_2']
+df = df[cols]
+df.to_csv(fname, index=False)
 
