1d5c2fd081a9dbd0b6c437551b47618555185f65
filippob <filippo.biscarini@gmail.com>
2023-01-02 22:21:57 +0100

    fixed issue with bgzip compression

--- bash_scripts/filter_vcf.sh
+++ bash_scripts/filter_vcf.sh
@@ -18,7 +18,7 @@ vcftools_img='/home/core/nxf_singularity_cache/depot.galaxyproject.org-singulari
 ### parameters
 thin=10000 ## distance between adjacent sites
 maf=0.05 ## 0.05 --> 5%
-mac=8 ## min. number of allele counts
+mac=4 ## min. number of allele counts
 minDP=0.25 ## average coverage across all samples
 maxmiss=0.5 ## max missing rate per site
 
@@ -52,22 +52,25 @@ if [ -f "$outfile" ]; then
 
 else
 	echo " - extracting only biallelic SNPs from the input vcf file"
-	singularity run -B /home/freeclimb/:/home/freeclimb/ ${vcftools_img} vcftools --gzvcf ${input_file} --remove-indels --thin $thin --maf $maf --mac $mac --max-missing $maxmiss --min-meanDP $minDP --recode-bcf --recode-INFO-all --out $outfile
-
+	#singularity run -B /home/freeclimb/:/home/freeclimb/ ${bcftools_img} bcftools view -e 'QUAL <= 1 || INFO/DP > 35 || INFO/DP < 0.2' -c $mac -q $maf ${input_file} | bgzip > $outfile
+	singularity run -B /home/freeclimb/:/home/freeclimb/ ${vcftools_img} vcftools --gzvcf ${input_file} --remove-indels --thin $thin --maf $maf --mac $mac --max-missing $maxmiss --min-meanDP $minDP --recode --recode-INFO-all --out $outfile
+	echo "compressing vcf file wth bgzip"
+	singularity run -B /home/freeclimb/:/home/freeclimb/ ${bcftools_img} bgzip -c $outfile.recode.vcf > $outfile.vcf.gz
 	echo " - indexing the subset vcf file with tabix"
-	singularity run -B /home/freeclimb/:/home/freeclimb/ ${bcftools_img} tabix ${outfile}.recode.bcf
+	singularity run -B /home/freeclimb/:/home/freeclimb/ ${bcftools_img} tabix ${outfile}.vcf.gz
+	rm ${outfile}.recode.vcf
 fi
 
 ## counting n. of samples
-nsamples=`singularity run -B /home/freeclimb/:/home/freeclimb/ ${bcftools_img} bcftools query -l $outfile | wc -l`
+nsamples=`singularity run -B /home/freeclimb/:/home/freeclimb/ ${bcftools_img} bcftools query -l $outfile.vcf.gz | wc -l`
 echo "n. of output samples from $outfile is $nsamples"
 
 ## counting n. of variants
-nvars=`singularity run -B /home/freeclimb/:/home/freeclimb/ ${bcftools_img} bcftools query -f '%POS\n' $outfile | wc -l`
+nvars=`singularity run -B /home/freeclimb/:/home/freeclimb/ ${bcftools_img} bcftools query -f '%POS\n' $outfile.vcf.gz | wc -l`
 echo "n. of output variants from $outfile is $nvars"
 
 ## stat report from bcftools
 echo " - calculating stats on filtered file ... "
-singularity run -B /home/freeclimb/:/home/freeclimb/ ${bcftools_img} bcftools stats $outfile.recode.bcf > ${outdir}/bcftools.filtered.stats
+singularity run -B /home/freeclimb/:/home/freeclimb/ ${bcftools_img} bcftools stats $outfile.vcf.gz > ${outdir}/bcftools.filtered.stats
 
 echo "DONE!!"
