3e519e9690c5b3f975b3f28dd5c1c71ab2c222a3
filippob <filippo.biscarini@gmail.com>
2023-01-09 10:42:16 +0100

    commenting start k value (k = 2)

--- bash_scripts/get_admixture_results.sh
+++ bash_scripts/get_admixture_results.sh
@@ -11,8 +11,8 @@ cd $outdir
 #get the cross-validation error (-h avoids the file name to be printed each time)
 grep -h "CV" log*.out > CVerr
 
-#get the number of iterations to reach convergence
-for (( i=1; i<=$k; i++ )); do cat log$i.out | awk '{if (NF==8 && $2=="(QN/Block)") print $1}' | tail -1; done > niter
+#get the number of iterations to reach convergence (start from 2, 1 cluster makes no sense - unless it was run by mistake in admixture.sh)
+for (( i=2; i<=$k; i++ )); do cat log$i.out | awk '{if (NF==8 && $2=="(QN/Block)") print $1}' | tail -1; done > niter
 
 echo "DONE!"
 
