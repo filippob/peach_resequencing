7ffb2d2619ad6bee93934396a3d72346f0ddbe3f
filippob <filippo.biscarini@gmail.com>
2023-01-24 18:53:11 +0100

    latest modifications (next: understand and parse output)

--- r_scripts/GXE_bglr_case1_markers.R
+++ r_scripts/GXE_bglr_case1_markers.R
@@ -63,31 +63,49 @@ X_main <- matrix(rep(t(X), ntraits), ncol=ncol(X),byrow=TRUE)
 
 for (i in 1:ntraits) {
   
-  print(i)
-  temp = matrix(rep(t(X0), i), ncol=ncol(X0),byrow=TRUE)
+  n = ntraits
+  print(paste(i,n))
+  temp1 = matrix(rep(t(X0), i-1), ncol=ncol(X0),byrow=TRUE)
+  temp2 = matrix(rep(t(X0), n-i), ncol=ncol(X0),byrow=TRUE)
+  temp = rbind(temp1, X, temp2)
   assign(paste("X", i, sep = "_"), temp)
 }
 
-X_1 = rbind(X,X0)   # (1198 x 1279) X + X0
-X_2 = rbind(X0,X)   # (1198 x 1279) X0 + X  
+print("Writing out example data")
+dir.create(file.path(config$base_folder, config$outdir), recursive = TRUE, showWarnings = FALSE)
 
 outpath = file.path(config$base_folder, config$outdir, "GxE_")
-fm = BGLR(y=y,ETA=list(             
+fm = BGLR(y=y$value,ETA=list(             
   main=list(X=X_main,model='BRR'),
   int1=list(X=X_1,model='BRR'),
-  int2=list(X=X_2,model='BRR')),
+  int2=list(X=X_2,model='BRR'),
+  int3=list(X=X_3,model='BRR'),
+  int4=list(X=X_4,model='BRR')
+  ),
   nIter=config$nIter, burnIn=config$burnIn, saveAt=outpath, groups=rep(1:ntraits,each=nrow(X))
 )
 
-varU_main=scan('GxE_ETA_main_varB.dat')[-c(1:200)] #1000 ?
-varU_int1=scan('GxE_ETA_int1_varB.dat')[-c(1:200)] #1000 ?
-varU_int2=scan('GxE_ETA_int2_varB.dat')[-c(1:200)] #1000 ?
-varE=read.table('GxE_varE.dat',header=FALSE)[-c(1:200),]  # 1000, from 201 to 1200 ?
+## hic sunt leones
+varU_main=scan(file.path(config$base_folder, config$outdir,'GxE_ETA_main_varB.dat'))[-c(1:200)] #1000 ?
+varU_int1=scan(file.path(config$base_folder, config$outdir,'GxE_ETA_int1_varB.dat'))[-c(1:200)] #1000 ?
+varU_int2=scan(file.path(config$base_folder, config$outdir,'GxE_ETA_int2_varB.dat'))[-c(1:200)] #1000 ?
+varU_int3=scan(file.path(config$base_folder, config$outdir,'GxE_ETA_int3_varB.dat'))[-c(1:200)] #1000 ?
+varU_int4=scan(file.path(config$base_folder, config$outdir,'GxE_ETA_int4_varB.dat'))[-c(1:200)] #1000 ?
+
+varE=read.table(file.path(config$base_folder, config$outdir,'GxE_varE.dat'),header=FALSE)[-c(1:200),]  # 1000, from 201 to 1200 ?
 varU1=varU_main+varU_int1
 varU2=varU_main+varU_int2
+varU3=varU_main+varU_int3
+varU4=varU_main+varU_int4
+
 h2_1=varU1/(varU1+varE[,1])
 h2_2=varU2/(varU2+varE[,2])
-COR=varU_main/sqrt(varU1*varU2)
+h2_3=varU3/(varU3+varE[,3])
+h2_4=varU4/(varU4+varE[,4])
+
+m <- cbind.data.frame(varU1, varU2, varU3, varU4)
+sqrt(m)
+COR=varU_main/sqrt(varU1*varU2*varU3*varU4)
 mean(h2_1)
 mean(h2_2)
 mean(COR)
