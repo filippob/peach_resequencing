36bfe6e1d2aab11554149081a1d64015e8c893c2
filippob <filippo.biscarini@gmail.com>
2023-02-28 19:13:35 +0100

    adding genetic correlations

--- r_scripts/parse_bglr_results.r
+++ r_scripts/parse_bglr_results.r
@@ -31,7 +31,8 @@ library("data.table")
 
 traits = strsplit(config$traits, ",")[[1]]
 
-res = data.frame("trait"=NULL, "test_corr"=NULL, "train_corr"=NULL, "h2_1"=NULL, "h2_2"=NULL, "h2_3"=NULL, "h2_4"=NULL)
+res = data.frame("trait"=NULL, "test_corr"=NULL, "train_corr"=NULL, "h2_1"=NULL, "h2_2"=NULL, "h2_3"=NULL, "h2_4"=NULL,
+                 "rg1_2"=NULL, "rg1_3"=NULL, "rg1_4"=NULL, "rg2_3"=NULL,"rg2_4"=NULL, "rg3_4"=NULL)
 for (trt in traits) {
   
   trt = str_trim(trt, side = "both")
@@ -76,13 +77,30 @@ for (trt in traits) {
     assign(paste("varU_int", i, sep = ""), temp)
     assign(paste("varU", i, sep=""), get(paste("varU_int", i, sep = "")) + varU_main)
     assign(paste("h2", i, sep="_"), get(paste("varU", i, sep=""))/(get(paste("varU", i, sep=""))+varE[,i]))
+    
+    
+  }
+  
+  ## pairwise combinations
+  lcomb = split(combn(length(filenames),2), rep(1:ncol(m), each = nrow(m)))
+  
+  for (i in 1:length(lcomb)) {
+    
+    tmp = varU_main/sqrt(get(paste("varU",lcomb[[i]][1],sep="")) * get(paste("varU",lcomb[[i]][2],sep="")))
+    assign(paste("rg",lcomb[[i]][1],lcomb[[i]][2],sep="_"), tmp)
   }
   
   temp = data.frame("trait"=trt, "test_corr"=test_corr, "train_corr"=train_corr,
                     "h2_1" = ifelse(exists("h2_1"),mean(h2_1),NA),
                     "h2_2" = ifelse(exists("h2_2"),mean(h2_2),NA),
                     "h2_3" = ifelse(exists("h2_3"),mean(h2_3),NA),
-                    "h2_4" = ifelse(exists("h2_4"),mean(h2_4),NA)
+                    "h2_4" = ifelse(exists("h2_4"),mean(h2_4),NA),
+                    "rg1_2" = ifelse(exists("rg_1_2"),mean(rg_1_2),NA),
+                    "rg1_3" = ifelse(exists("rg_1_3"),mean(rg_1_3),NA),
+                    "rg1_4" = ifelse(exists("rg_1_4"),mean(rg_1_4),NA),
+                    "rg2_3" = ifelse(exists("rg_2_3"),mean(rg_2_3),NA),
+                    "rg2_4" = ifelse(exists("rg_2_4"),mean(rg_2_4),NA),
+                    "rg3_4" = ifelse(exists("rg_3_4"),mean(rg_3_4),NA)
                     )
   res = rbind.data.frame(res,temp)
   
@@ -96,5 +114,14 @@ fname = file.path(config$base_folder, config$outdir, "results.csv")
 fwrite(x = res, file = fname, sep=",")
 
 print("DONE!!")
+
+CORg_1_2=varU_main/sqrt(varU1*varU2)
+CORg_1_3=varU_main/sqrt(varU1*varU3)
+CORg_1_4=varU_main/sqrt(varU1*varU4)
+CORg_2_3=varU_main/sqrt(varU2*varU3)
+CORg_2_4=varU_main/sqrt(varU2*varU4)
+CORg_3_4=varU_main/sqrt(varU3*varU4)
+CORg=varU_main/sqrt(varU1*varU2*varU3*varU4)
+
 
 
